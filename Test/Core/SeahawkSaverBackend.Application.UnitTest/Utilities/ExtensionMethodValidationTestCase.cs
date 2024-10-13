ed20d8f3219d095f8c3d422616c4387f9e985f67
TateHouse <tatemonroehouse@gmail.com>
2024-10-12 21:30:51 -0400

    Test: Extension method validation test

---
+++ Test/Core/SeahawkSaverBackend.Application.UnitTest/Utilities/ExtensionMethodValidationTestCase.cs
@@ -0,0 +1,12 @@
+﻿namespace SeahawkSaverBackend.Application.UnitTest.Utilities;
+/**
+ * <summary>
+ * A data transfer object for validation extension method test cases.
+ * </summary>
+ */
+public sealed record ExtensionMethodValidationTestCase<TDTO>
+	where TDTO : class
+{
+	public required TDTO DTO { get; init; }
+	public string? ExpectedErrorPropertyName { get; init; }
+}
