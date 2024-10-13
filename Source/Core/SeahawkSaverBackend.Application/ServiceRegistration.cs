831e3b32e681d17cd65bd2a022266d1351f38595
TateHouse <tatemonroehouse@gmail.com>
2024-10-12 20:53:44 -0400

    Feature: Register application services

---
+++ Source/Core/SeahawkSaverBackend.Application/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿namespace SeahawkSaverBackend.Application;
+using Microsoft.Extensions.DependencyInjection;
+
+/**
+ * <summary>
+ * A class for registering services.
+ * </summary>
+ */
+public static class ServiceRegistration
+{
+	/**
+	 * <summary>
+	 * An extension method for <see cref="IServiceCollection"/> to register the <see cref="Application"/> services.
+	 * </summary>
+	 */
+	public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+	{
+		services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+		services.AddMediatR(configuration =>
+		{
+			configuration.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+		});
+
+		return services;
+	}
+}
