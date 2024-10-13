3ee66e3488a9d6f814beab82f6833e01871387af
TateHouse <tatemonroehouse@gmail.com>
2024-10-12 20:47:22 -0400

    Feature: Register persistence services

---
+++ Source/Infrastructure/SeahawkSaverBackend.Persistence/DatabaseSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace SeahawkSaverBackend.Persistence;
+/**
+ * <summary>
+ * The database settings.
+ * </summary>
+ */
+public sealed record DatabaseSettings
+{
+	/**
+	 * <summary>
+	 * The name of the database.
+	 * </summary>
+	 */
+	public string Name { get; init; }
+
+	/**
+	 * <summary>
+	 * The name of the database provider.
+	 * </summary>
+	 */
+	public string Provider { get; init; }
+
+	public DatabaseSettings(string name, string provider)
+	{
+		if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(provider))
+		{
+			throw new ArgumentException("The Database:Name and Database:Provider must be provided.");
+		}
+
+		Name = name;
+		Provider = provider;
+	}
+}
---
+++ Source/Infrastructure/SeahawkSaverBackend.Persistence/ServiceRegistration.cs
@@ -0,0 +1,67 @@
+﻿namespace SeahawkSaverBackend.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+/**
+ * <summary>
+ * A class for registering services.
+ * </summary>
+ */
+public static class ServiceRegistration
+{
+	/**
+	 * <summary>
+	 * An extension method for <see cref="IServiceCollection"/> to register the <see cref="Persistence"/> services.
+	 * </summary>
+	 * <param name="configuration">The application's set of key/value pair configurations.</param>
+	 * <exception cref="ArgumentException">Thrown if the database name or provider is null or whitespace.</exception>
+	 * <exception cref="InvalidOperationException">Thrown if the database settings failed to parse.</exception>
+	 * <exception cref="NotSupportedException">Thrown if the database provider is not supported.</exception>
+	 */
+	public static IServiceCollection RegisterPersistenceServices(this IServiceCollection services,
+																 IConfiguration configuration)
+	{
+		var databaseSettings = configuration.GetSection("Database").Get<DatabaseSettings>();
+
+		if (databaseSettings == null)
+		{
+			throw new InvalidOperationException("At least one fo the provided database settings was invalid.");
+		}
+
+		services.AddDbContext<DatabaseContext>(optionsAction =>
+		{
+			ServiceRegistration.ConfigureDatabaseContext(optionsAction, databaseSettings);
+		});
+
+		return services;
+	}
+
+	/**
+	 * <summary>
+	 * Configures the application's Entity Framework Core <see cref="DatabaseContext"/>.
+	 * </summary>
+	 * <param name="optionsBuilder">The <see cref="DbContextOptionsBuilder"/>.</param>
+	 * <param name="databaseSettings">The database settings.</param>
+	 * <exception cref="NotSupportedException">Thrown if the database provider is not supported.</exception>
+	 */
+	private static void ConfigureDatabaseContext(DbContextOptionsBuilder optionsBuilder,
+												 DatabaseSettings databaseSettings)
+	{
+		switch (databaseSettings.Provider)
+		{
+			case "InMemory":
+				optionsBuilder.UseInMemoryDatabase(databaseSettings.Name);
+				optionsBuilder.ConfigureWarnings(warningsConfigurationBuilderAction =>
+				{
+					warningsConfigurationBuilderAction.Ignore(InMemoryEventId.TransactionIgnoredWarning);
+				});
+
+				break;
+
+			default:
+				throw new NotSupportedException($"Unsupported database provider: {databaseSettings.Provider}");
+		}
+	}
+}
