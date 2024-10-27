439d8804c2c2ce3b177e22fcf04bf4ed89491ce9
TateHouse <tatemonroehouse@gmail.com>
2024-10-27 18:57:11 -0400

    Feature: List income endpoint

---
+++ Source/API/SeahawkSaverBackend.API/Endpoints/Income/Queries/List/DTOs/ListIncomeEndpointIncomeResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace SeahawkSaverBackend.API.Endpoints.Income.Queries.List.DTOs;
+/**
+ * <summary>
+ * A data transfer object containing the <see cref="SeahawkSaverBackend.Domain.Entities.Income"/> data provided in the
+ * response for the <see cref="ListIncomeEndpoint"/>.
+ * </summary>
+ */
+public sealed record ListIncomeEndpointIncomeResponse
+{
+	public required Guid IncomeId { get; init; }
+	public required decimal Amount { get; init; }
+	public required DateTime DateTime { get; init; }
+}
---
+++ Source/API/SeahawkSaverBackend.API/Endpoints/Income/Queries/List/DTOs/ListIncomeEndpointResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SeahawkSaverBackend.API.Endpoints.Income.Queries.List.DTOs;
+
+/**
+ * <summary>
+ * A data transfer object containing the data returned by the <see cref="ListIncomeEndpoint"/>.
+ * </summary>
+ */
+public sealed record ListIncomeEndpointResponse
+{
+	public required IReadOnlyList<ListIncomeEndpointIncomeResponse> Incomes { get; init; }
+}
