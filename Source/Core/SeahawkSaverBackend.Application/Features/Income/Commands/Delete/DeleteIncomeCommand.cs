2558a33264bd13375e1082838ac882c2bad21398
TateHouse <tatemonroehouse@gmail.com>
2024-10-27 22:19:25 -0400

    Feature: Delete income handler

---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Income/Commands/Delete/DeleteIncomeCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Income.Commands.Delete;
+using MediatR;
+using SeahawkSaverBackend.Application.Abstractions.Application.Commands;
+
+/**
+ * <summary>
+ * A command for deleting a <see cref="SeahawkSaverBackend.Domain.Entities.Income"/> entity.
+ * </summary>
+ */
+public sealed class DeleteIncomeCommand : Command<Unit>
+{
+	public required Guid IncomeId { get; init; }
+	public required Guid UserId { get; init; }
+}
---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Income/Commands/Delete/DeleteIncomeCommandFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Income.Commands.Delete;
+using SeahawkSaverBackend.Application.Abstractions.Application.Commands;
+
+/**
+ * <summary>
+ * A factory for the <see cref="DeleteIncomeCommand"/>.
+ * </summary>
+ */
+public static class DeleteIncomeCommandFactory
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="DeleteIncomeCommand"/> instance.
+	 * </summary>
+	 * <param name="commandSettings">The command settings.</param>
+	 * <param name="incomeId">The id of the income to delete.</param>
+	 * <param name="userId">The id of the associate user.</param>
+	 * <returns>A new <see cref="DeleteIncomeCommand"/> isntance.</returns>
+	 */
+	public static DeleteIncomeCommand Create(CommandSettings commandSettings, Guid incomeId, Guid userId)
+	{
+		return new DeleteIncomeCommand
+		{
+			CommandSettings = commandSettings,
+			IncomeId = incomeId,
+			UserId = userId
+		};
+	}
+}
