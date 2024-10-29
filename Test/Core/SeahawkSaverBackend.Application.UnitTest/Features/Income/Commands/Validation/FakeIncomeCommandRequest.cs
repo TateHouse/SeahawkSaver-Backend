72832a564ce3879396082c70556cc3a24967708e
TateHouse <tatemonroehouse@gmail.com>
2024-10-29 12:09:13 -0400

    Feature: Income validation rules
    Two additional income related validation rules were added. One for the
    associated user id and one for the income's id.

--- Test/Core/SeahawkSaverBackend.Application.UnitTest/Features/Income/Commands/Validation/FakeIncomeCommandRequest.cs
+++ Test/Core/SeahawkSaverBackend.Application.UnitTest/Features/Income/Commands/Validation/FakeIncomeCommandRequest.cs
@@ -3,6 +3,8 @@ using SeahawkSaverBackend.Application.Features.Income.Commands;
 
 public sealed record FakeIncomeCommandRequest : IncomeCommandRequest
 {
+	public Guid? UserId { get; set; }
+	public Guid? IncomeId { get; set; }
 	public decimal? Amount { get; set; }
 	public DateTime? DateTime { get; set; }
 }
