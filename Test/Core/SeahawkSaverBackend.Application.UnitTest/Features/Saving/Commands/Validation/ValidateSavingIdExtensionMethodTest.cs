481a27601f1f6eee47b6176ca872be67cb726536
TateHouse <tatemonroehouse@gmail.com>
2024-11-06 00:34:14 -0500

    Feature: Saving validation rules

---
+++ Test/Core/SeahawkSaverBackend.Application.UnitTest/Features/Saving/Commands/Validation/ValidateSavingIdExtensionMethodTest.cs
@@ -0,0 +1,63 @@
+﻿namespace SeahawkSaverBackend.Application.UnitTest.Features.Saving.Commands.Validation;
+using FluentValidation;
+using SeahawkSaverBackend.Application.Features.Saving.Commands;
+using SeahawkSaverBackend.Application.UnitTest.Utilities;
+
+[TestFixture]
+public sealed class ValidateSavingIdExtensionMethodTest : ExtensionMethodValidationTest<FakeSavingCommandRequest>
+{
+	protected override InlineValidator<FakeSavingCommandRequest> CreateValidator()
+	{
+		return new InlineValidator<FakeSavingCommandRequest>();
+	}
+
+	public override void SetUp()
+	{
+		base.SetUp();
+		Validator.RuleFor(saving => saving.SavingId)
+				 .ValidateSavingId();
+	}
+
+	[Test]
+	[TestCaseSource(nameof(InvalidDTOs))]
+	public override void GivenInvalidDTO_WhenValidate_ThenValidationResultContainsValidationError(ExtensionMethodValidationTestCase<FakeSavingCommandRequest> testCase)
+	{
+		base.GivenInvalidDTO_WhenValidate_ThenValidationResultContainsValidationError(testCase);
+	}
+
+	[Test]
+	[TestCaseSource(nameof(ValidDTOs))]
+	public override void GivenValidDTO_WhenValidate_ThenValidationResultContainsNoValidationErrors(ExtensionMethodValidationTestCase<FakeSavingCommandRequest> testCase)
+	{
+		base.GivenValidDTO_WhenValidate_ThenValidationResultContainsNoValidationErrors(testCase);
+	}
+
+	public static IEnumerable<ExtensionMethodValidationTestCase<FakeSavingCommandRequest>> InvalidDTOs
+	{
+		get
+		{
+			yield return new ExtensionMethodValidationTestCase<FakeSavingCommandRequest>
+			{
+				DTO = new FakeSavingCommandRequest
+				{
+					SavingId = Guid.Empty
+				},
+				ExpectedErrorPropertyName = nameof(FakeSavingCommandRequest.SavingId)
+			};
+		}
+	}
+
+	public static IEnumerable<ExtensionMethodValidationTestCase<FakeSavingCommandRequest>> ValidDTOs
+	{
+		get
+		{
+			yield return new ExtensionMethodValidationTestCase<FakeSavingCommandRequest>
+			{
+				DTO = new FakeSavingCommandRequest
+				{
+					SavingId = Guid.NewGuid()
+				}
+			};
+		}
+	}
+}
