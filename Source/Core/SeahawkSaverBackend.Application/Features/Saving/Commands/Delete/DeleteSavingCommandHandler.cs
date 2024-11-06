af1b1b423457da3239059512270f3622a74e3d1a
TateHouse <tatemonroehouse@gmail.com>
2024-11-05 23:01:58 -0500

    Feature: Delete saving handler

---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Saving/Commands/Delete/DeleteSavingCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Saving.Commands.Delete;
+using MediatR;
+using SeahawkSaverBackend.Application.Abstractions.Application.Commands;
+using SeahawkSaverBackend.Application.Abstractions.Persistence.Transactions;
+using SeahawkSaverBackend.Application.Exceptions;
+using SeahawkSaverBackend.Application.Features.Saving.Queries.Specifications;
+using SeahawkSaverBackend.Domain.Entities;
+
+/**
+ * <summary>
+ * The command handler for the <see cref="DeleteSavingCommand"/>.
+ * </summary>
+ */
+public sealed class DeleteSavingCommandHandler : CommandHandler<DeleteSavingCommand, Unit>
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="DeleteSavingCommandHandler"/> instance.
+	 * </summary>
+	 * <param name="transaction">The "unit of work" used during the command execution.</param>
+	 */
+	public DeleteSavingCommandHandler(ICommandTransaction transaction)
+		: base(transaction, null)
+	{
+
+	}
+
+	protected override async Task<Unit> HandleAsync(DeleteSavingCommand request, CancellationToken cancellationToken)
+	{
+		var specification = new GetSavingByIdSpecification(request.SavingId, request.UserId);
+		var saving = await Transaction.SavingRepository.SingleOrDefaultAsync(specification, cancellationToken);
+
+		if (saving == null)
+		{
+			throw new NotFoundException(nameof(Saving), request.SavingId);
+		}
+
+		await Transaction.SavingRepository.DeleteAsync(saving, cancellationToken);
+
+		return Unit.Value;
+	}
+}
---
+++ Test/Core/SeahawkSaverBackend.Application.UnitTest/Features/Saving/Commands/Delete/DeleteSavingCommandHandlerTest.cs
@@ -0,0 +1,58 @@
+﻿namespace SeahawkSaverBackend.Application.UnitTest.Features.Saving.Commands.Delete;
+using Ardalis.Specification;
+using MediatR;
+using Moq;
+using SeahawkSaverBackend.Application.Abstractions.Application.Commands;
+using SeahawkSaverBackend.Application.Abstractions.Persistence.Transactions;
+using SeahawkSaverBackend.Application.Exceptions;
+using SeahawkSaverBackend.Application.Features.Saving.Commands.Delete;
+using SeahawkSaverBackend.Domain.Entities;
+using SeahawkSaverBackend.Domain.Factories;
+
+[TestFixture]
+public sealed class DeleteSavingCommandHandlerTest
+{
+	private Mock<ICommandTransaction> mockTransaction;
+	private DeleteSavingCommandHandler commandHandler;
+	private CommandSettings commandSettings;
+
+	[SetUp]
+	public void SetUp()
+	{
+		mockTransaction = new Mock<ICommandTransaction>();
+		commandHandler = new DeleteSavingCommandHandler(mockTransaction.Object);
+		commandSettings = new CommandSettings(true, true);
+	}
+
+	[Test]
+	public async Task GivenSavingIdThatDoesNotExistAndUserIdThatDoesNotExist_WhenHandle_ThenThrowsNotFoundException()
+	{
+		mockTransaction.Setup(mock => mock.SavingRepository.SingleOrDefaultAsync(It.IsAny<ISingleResultSpecification<Saving>>(), It.IsAny<CancellationToken>()))
+					   .ReturnsAsync(() => null);
+
+		var request = DeleteSavingCommandFactory.Create(commandSettings, Guid.NewGuid(), Guid.NewGuid());
+
+		Assert.ThrowsAsync<NotFoundException>(() => commandHandler.Handle(request, CancellationToken.None));
+
+		mockTransaction.Verify(mock => mock.SavingRepository.SingleOrDefaultAsync(It.IsAny<ISingleResultSpecification<Saving>>(), It.IsAny<CancellationToken>()), Times.Once);
+	}
+
+	[Test]
+	public async Task GivenSavingIdThatExistsAndUserIdThatExists_WhenHandle_ThenReturnsUnit()
+	{
+		var saving = SavingFactory.Create(Guid.NewGuid(), 100, DateTime.Now.AddDays(-1), Guid.NewGuid());
+
+		mockTransaction.Setup(mock => mock.SavingRepository.SingleOrDefaultAsync(It.IsAny<ISingleResultSpecification<Saving>>(), It.IsAny<CancellationToken>()))
+					   .ReturnsAsync(saving);
+
+		mockTransaction.Setup(mock => mock.SavingRepository.DeleteAsync(It.IsAny<Saving>(), It.IsAny<CancellationToken>()));
+
+		var request = DeleteSavingCommandFactory.Create(commandSettings, saving.SavingId, saving.UserId);
+		var response = await commandHandler.Handle(request, CancellationToken.None);
+
+		Assert.That(response, Is.TypeOf<Unit>());
+
+		mockTransaction.Verify(mock => mock.SavingRepository.SingleOrDefaultAsync(It.IsAny<ISingleResultSpecification<Saving>>(), It.IsAny<CancellationToken>()), Times.Once);
+		mockTransaction.Verify(mock => mock.SavingRepository.DeleteAsync(It.IsAny<Saving>(), It.IsAny<CancellationToken>()), Times.Once);
+	}
+}
