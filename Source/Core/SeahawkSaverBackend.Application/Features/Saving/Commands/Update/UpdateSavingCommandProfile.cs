09de6f3f647ce5161a0fdfa313fbaf90d5718ab9
TateHouse <tatemonroehouse@gmail.com>
2024-11-06 21:39:14 -0500

    Feature: Update saving handler

---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Saving/Commands/Update/UpdateSavingCommandProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Saving.Commands.Update;
+using AutoMapper;
+using SeahawkSaverBackend.Application.Features.Saving.Commands.Update.DTOs;
+using SeahawkSaverBackend.Domain.Entities;
+
+/**
+ * <summary>
+ * The AutoMapper <see cref="Profile"/> for the <see cref="UpdateSavingCommand"/>.
+ * </summary>
+ */
+public sealed class UpdateSavingCommandProfile : Profile
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="UpdateSavingCommandProfile"/> instance.
+	 * </summary>
+	 */
+	public UpdateSavingCommandProfile()
+	{
+		CreateMap<UpdateSavingCommandSavingRequest, Saving>()
+			.ForMember(destinationMember => destinationMember.SavingId,
+					   memberOptions => memberOptions.Ignore())
+			.ForMember(destinationMember => destinationMember.UserId,
+					   memberOptions => memberOptions.Ignore());
+	}
+}
