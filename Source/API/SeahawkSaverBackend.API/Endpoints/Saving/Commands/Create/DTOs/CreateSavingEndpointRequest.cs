642af1fc72dd70b553f864a51d0019d638412d41
TateHouse <tatemonroehouse@gmail.com>
2024-11-06 21:22:43 -0500

    Feature: Create saving endpoint

---
+++ Source/API/SeahawkSaverBackend.API/Endpoints/Saving/Commands/Create/DTOs/CreateSavingEndpointRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SeahawkSaverBackend.API.Endpoints.Saving.Commands.Create.DTOs;
+/**
+ * <summary>
+ * A data transfer object containing the data for the <see cref="CreateSavingEndpoint"/>.
+ * </summary>
+ */
+public sealed record CreateSavingEndpointRequest
+{
+	public required CreateSavingEndpointSavingRequest Saving { get; init; }
+}
