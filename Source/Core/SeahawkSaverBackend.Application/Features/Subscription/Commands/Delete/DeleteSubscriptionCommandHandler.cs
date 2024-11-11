51a6dcaad971ec3e051c8b2420e49fc861a56117
TateHouse <tatemonroehouse@gmail.com>
2024-11-11 17:48:34 -0500

    Feature: Delete subscription handler

---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Subscription/Commands/Delete/DeleteSubscriptionCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Subscription.Commands.Delete;
+using MediatR;
+using SeahawkSaverBackend.Application.Abstractions.Application.Commands;
+using SeahawkSaverBackend.Application.Abstractions.Persistence.Transactions;
+using SeahawkSaverBackend.Application.Exceptions;
+using SeahawkSaverBackend.Application.Features.Subscription.Queries.Specifications;
+using SeahawkSaverBackend.Domain.Entities;
+
+/**
+ * <summary>
+ * The command handler for the <see cref="DeleteSubscriptionCommand"/>.
+ * </summary>
+ */
+public sealed class DeleteSubscriptionCommandHandler : CommandHandler<DeleteSubscriptionCommand, Unit>
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="DeleteSubscriptionCommandHandler"/> instance.
+	 * </summary>
+	 * <param name="transaction">The "unit of work" used during the command execution.</param>
+	 */
+	public DeleteSubscriptionCommandHandler(ICommandTransaction transaction)
+		: base(transaction, null)
+	{
+
+	}
+
+	protected override async Task<Unit> HandleAsync(DeleteSubscriptionCommand request, CancellationToken cancellationToken)
+	{
+		var specification = new GetSubscriptionByIdSpecification(request.SubscriptionId, request.UserId);
+		var subscription = await Transaction.SubscriptionRepository.SingleOrDefaultAsync(specification, cancellationToken);
+
+		if (subscription == null)
+		{
+			throw new NotFoundException(nameof(Subscription), request.SubscriptionId);
+		}
+
+		await Transaction.SubscriptionRepository.DeleteAsync(subscription, cancellationToken);
+
+		return Unit.Value;
+	}
+}
---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Subscription/Queries/Specifications/GetSubscriptionByIdSpecification.cs
@@ -0,0 +1,25 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Subscription.Queries.Specifications;
+using Ardalis.Specification;
+using SeahawkSaverBackend.Domain.Entities;
+
+/**
+ * <summary>
+ * A specification for retrieving a <see cref="Subscription"/> entity from the database by its
+ * <see cref="Subscription.SubscriptionId"/> and its associated <see cref="User.UserId"/>.
+ * </summary>
+ */
+public sealed class GetSubscriptionByIdSpecification : SingleResultSpecification<Subscription>
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="GetSubscriptionByIdSpecification"/> instance.
+	 * </summary>
+	 * <param name="subscriptionId">The subscription's id.</param>
+	 * <param name="userId">The id of the associated user.</param>
+	 */
+	public GetSubscriptionByIdSpecification(Guid subscriptionId, Guid userId)
+	{
+		Query.Where(subscription => subscription.SubscriptionId == subscriptionId &&
+									subscription.UserId == userId);
+	}
+}
