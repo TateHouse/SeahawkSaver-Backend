b7ca16a01fb3358f6862b8627ff74eefde867bec
TateHouse <tatemonroehouse@gmail.com>
2024-11-11 18:06:59 -0500

    Feature: List subscription handler

---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Subscription/Queries/List/DTOs/ListSubscriptionQueryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Subscription.Queries.List.DTOs;
+/**
+ * <summary>
+ * A data transfer object containing the data returned by the <see cref="ListSubscriptionQuery"/>.
+ * </summary>
+ */
+public sealed record ListSubscriptionQueryResponse
+{
+	public required IReadOnlyList<ListSubscriptionQueryResponse> Subscriptions { get; init; }
+}
---
+++ Source/Core/SeahawkSaverBackend.Application/Features/Subscription/Queries/List/ListSubscriptionQueryProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace SeahawkSaverBackend.Application.Features.Subscription.Queries.List;
+using AutoMapper;
+using SeahawkSaverBackend.Application.Features.Subscription.Queries.List.DTOs;
+using SeahawkSaverBackend.Domain.Entities;
+
+/**
+ * <summary>
+ * The AutoMapper <see cref="Profile"/> for the <see cref="ListSubscriptionQuery"/>.
+ * </summary>
+ */
+public sealed class ListSubscriptionQueryProfile : Profile
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="ListSubscriptionQueryProfile"/> instance.
+	 * </summary>
+	 */
+	public ListSubscriptionQueryProfile()
+	{
+		CreateMap<Subscription, ListSubscriptionQuerySubscriptionResponse>();
+		CreateMap<IEnumerable<Subscription>, ListSubscriptionQueryResponse>()
+			.ForMember(destinationMember => destinationMember.Subscriptions,
+					   memberOptions => memberOptions.MapFrom(sourceMember => sourceMember));
+	}
+}
