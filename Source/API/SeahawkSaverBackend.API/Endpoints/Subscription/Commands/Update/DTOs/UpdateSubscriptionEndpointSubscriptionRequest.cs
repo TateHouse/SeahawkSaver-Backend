907a68513ee3373a6fab1dc453f7f9466c851737
TateHouse <tatemonroehouse@gmail.com>
2024-11-11 18:40:02 -0500

    Feature: Update subscription endpoint

---
+++ Source/API/SeahawkSaverBackend.API/Endpoints/Subscription/Commands/Update/DTOs/UpdateSubscriptionEndpointSubscriptionRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SeahawkSaverBackend.API.Endpoints.Subscription.Commands.Update.DTOs;
+/**
+ * <summary>
+ * A data transfer object containing the <see cref="SeahawkSaverBackend.Domain.Entities.Subscription"/> data provided in
+ * the request for the <see cref="UpdateSubscriptionEndpoint"/>.
+ * </summary>
+ */
+public sealed record UpdateSubscriptionEndpointSubscriptionRequest
+{
+	public required Guid SubscriptionId { get; init; }
+	public required decimal Amount { get; init; }
+	public required DateTime DateTime { get; init; }
+}
--- Source/API/SeahawkSaverBackend.API/Endpoints/Subscription/SubscriptionEndpointsMapper.cs
+++ Source/API/SeahawkSaverBackend.API/Endpoints/Subscription/SubscriptionEndpointsMapper.cs
@@ -1,6 +1,7 @@
 namespace SeahawkSaverBackend.API.Endpoints.Subscription;
 using SeahawkSaverBackend.API.Endpoints.Subscription.Commands.Create;
 using SeahawkSaverBackend.API.Endpoints.Subscription.Commands.Delete;
+using SeahawkSaverBackend.API.Endpoints.Subscription.Commands.Update;
 using SeahawkSaverBackend.API.Endpoints.Subscription.Queries.List;
 
 /**
@@ -28,6 +29,7 @@ public static class SubscriptionEndpointsMapper
 	{
 		var groupBuilder = application.MapGroup(SubscriptionEndpointsMapper.Prefix);
         CreateSubscriptionEndpoint.MapEndpoint(groupBuilder, SubscriptionEndpointsMapper.Tags);
+		UpdateSubscriptionEndpoint.MapEndpoint(groupBuilder, SubscriptionEndpointsMapper.Tags);
 		DeleteSubscriptionEndpoint.MapEndpoint(groupBuilder, SubscriptionEndpointsMapper.Tags);
 		ListSubscriptionEndpoint.MapEndpoint(groupBuilder, SubscriptionEndpointsMapper.Tags);
 	}
